36828ea7b093a46273cd2dcba57c3390f01f6a8e
Sravan Kumar Reddy Bontha <bonthasravan@gmail.com>
2018-06-10 10:39:30 +0530

    Add files via upload

---
+++ accordian.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $(".Intro").click(function(){
+        $(".What").toggle(250);
+    }
+    );
+    $(".Body").click(function(){
+        $(".Why").toggle(250);
+    }
+);$(".Conclusion").click(function(){
+        $(".Where").toggle(250);
+    }
+    );
+
+});
